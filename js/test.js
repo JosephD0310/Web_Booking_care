23fa3a8b3ff8edf444d9739987d96f5caef48255
Đặng Hải Linh <112225166+JosephD0310@users.noreply.github.com>
2023-07-05 23:56:49 +0700

    update umg

--- js/test.js
+++ js/test.js
@@ -1,23 +1,6 @@
-var dateInput = document.getElementById("date");
-var timeSelect = document.getElementById("time");
+
 var ticketInfo = document.getElementById("ticketInfo");
-var selectedDate = document.getElementById("selectedDate");
-var selectedTime = document.getElementById("selectedTime");
 
-dateInput.addEventListener("input", function() {
-  // Reset giá trị lựa chọn giờ và ẩn thông tin vé
-  timeSelect.value = "";
-  ticketInfo.style.display = "none";
-});
-
-timeSelect.addEventListener("change", function() {
-  var date = dateInput.value;
-  var time = timeSelect.value;
-
-  if (date && time) {
-    // Hiển thị thông tin vé
-    selectedDate.textContent = "Ngày: " + date;
-    selectedTime.textContent = "Giờ: " + time;
-    ticketInfo.style.display = "block";
-  }
-});
+function show(){
+  ticketInfo.style.display = "block";
+}
