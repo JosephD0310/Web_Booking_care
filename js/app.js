3d8f954786db41af37369d1dcf56f1a90c64f322
Đặng Hải Linh <112225166+JosephD0310@users.noreply.github.com>
2023-07-08 23:51:03 +0700

    update home

--- js/app.js
+++ js/app.js
@@ -8,3 +8,52 @@ function stickyNavbar () {
     nav.classList.toggle("scrolled", window.pageYOffset > 0);
 }
 window.addEventListener("scroll", stickyNavbar)
+
+/* ---------------------  Menu bar --------------------- */
+
+let navlink = document.querySelector('header .nav-bar .container .links');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navlink.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navlink.classList.remove('active');
+}
+
+/* ---------------------  Swiper --------------------- */
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".doctor-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+  });
